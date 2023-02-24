f47dd87235a9f0e9f56d9e81593b5a6b8e05284c
WandirPereira <wandir@gmail.com>
2023-02-23 22:18:53 -0300

    Instalando o Entity Framework 8

--- CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -1,12 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Data
 {
-    public class StreamerDbContext
+    public class StreamerDbContext : DbContext
     {
-        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Server=localhost; Database=Streamer;User Id= sa;Password=senha@1234;Encrypt=False")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
     }
 }
